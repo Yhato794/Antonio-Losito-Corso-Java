876508787b5857b23427a7acad1b6c089e99c43f
Antonio Losito <Antonio794l@gmail.com>
2023-02-20 12:25:57 +0100

    Update StampaSequenza.java
    Stampa numeri pari

--- Fibonacci/StampaSequenza.java
+++ Fibonacci/StampaSequenza.java
@@ -5,14 +5,17 @@ public class StampaSequenza {
         System.out.println("Selezione numeri di sequenza");
         Scanner scelta = new Scanner(System.in);
         int loop = scelta.nextInt();
-        int n1 = 0;// Numero sequenza 0
-        int n2 = 1;// Numero sequenza 1
+        int n1 = 0;// Numero sequenza 
+        int n2 = 1;// Numero sequenza 
         int n3; //Numero Somma 0 + 1
         for (int i = 0; i < loop; i++) {
             n3 = n1 + n2; //somma numero attuale sequenza e numero precendente 
             n1 = n2; // Scambio numero posizione -2 con posizione -3
             n2 = n3; // Scambio numero posizione -1 con posione 0
-            System.out.println(n3);
+            
+             if(n3%2==0){
+                System.out.println("Numeri pari della serie: "+n3);
+             }
         }
         scelta.close();
     }
