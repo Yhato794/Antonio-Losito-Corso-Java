7da9c70a9892379d079098808a886684509c1c7b
Antonio Losito <Antonio794l@gmail.com>
2023-02-20 11:51:59 +0100

    Update StampaSequenza.java
    Rinominato File

--- Fibonacci/StampaSequenza.java
+++ Fibonacci/StampaSequenza.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+import java.util.Scanner;;
 
-public class EsercizioFibonacci {
+public class StampaSequenza {
     public static void main(String[] args) {
         System.out.println("Selezione numeri di sequenza");
         Scanner scelta = new Scanner(System.in);
@@ -10,8 +10,8 @@ public class EsercizioFibonacci {
         int n3; //Numero Somma 0 + 1
         for (int i = 0; i < loop; i++) {
             n3 = n1 + n2; //somma numero attuale sequenza e numero precendente 
-            n1 = n2; // Scambio numero posizione -1 con posizione -2 
-            n2 = n3; // Scambio numero posizione 0 con posione -1
+            n1 = n2; // Scambio numero posizione -2 con posizione -3
+            n2 = n3; // Scambio numero posizione -1 con posione 0
             System.out.println(n3);
         }
         scelta.close();
