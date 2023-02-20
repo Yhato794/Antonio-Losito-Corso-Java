116233b184fe2680e4acdcca933f4246c54c2ffe
Antonio Losito <Antonio794l@gmail.com>
2023-02-20 12:39:51 +0100

    Update StampaSequenza.java

--- Fibonacci/StampaSequenza.java
+++ Fibonacci/StampaSequenza.java
@@ -5,20 +5,19 @@ public class StampaSequenza {
         System.out.println("Selezione numeri di sequenza");
         Scanner scelta = new Scanner(System.in);
         int loop = scelta.nextInt();
-        int n1 = 0;// Numero sequenza 
-        int n2 = 1;// Numero sequenza 
-        int n3; //Numero Somma 0 + 1
+        int n1 = 0;// Numero sequenza
+        int n2 = 1;// Numero sequenza
+        int n3; // Numero Somma 0 + 1
         for (int i = 0; i < loop; i++) {
-            n3 = n1 + n2; //somma numero attuale sequenza e numero precendente 
+            n3 = n1 + n2; // somma numero attuale sequenza e numero precendente
             n1 = n2; // Scambio numero posizione -2 con posizione -3
             n2 = n3; // Scambio numero posizione -1 con posione 0
-            
-             if(n3%2==0){
-                System.out.println("Numeri pari della serie: "+n3);
-             }else {
-                System.out.println("Numeri dispari della serie: "+n3);
 
-             }
+            if (n3 % 2 == 0)
+                System.out.println(n3+" pari");
+            else
+                System.out.println(n3+" dispari");
+
         }
         scelta.close();
     }
