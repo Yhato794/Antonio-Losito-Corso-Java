45e0e5620f32a754ecb2f2a7dcbccfed43f13149
Antonio Losito <Antonio794l@gmail.com>
2023-03-09 15:29:31 +0100

    calc with try catch

---
+++ ScriptJava/Giorno 12 Polimorfismo/TryCatch/Esempio.java
@@ -0,0 +1,38 @@
+
+// import java.util.Scanner;
+
+// public class Esempio {
+//     public static Scanner interi = new Scanner(System.in);
+
+//     public static void main(String[] args) {
+//         boolean errore = true;
+//         int i=0;
+//         do {
+//             try {
+//                 i = interi.nextInt();
+//                 errore = false;
+//                 break;
+//             } catch (Exception e) {
+//                 System.out.println("ERRORE");
+//                 interi.nextLine();
+//                 errore = true;
+//             } finally {
+//                 if (errore)
+//                     System.out.println("Hai sbagliato");
+//                 else
+//                     System.out.println("Input corretto");
+//             }
+//         } while (errore);
+//         checkage(i);
+
+//     }
+
+//     static void checkage(int i){
+//         if(i<18){
+//             throw new ArithmeticException("Devi essere maggiorenne");
+            
+//         }else{
+//             System.out.println("Sei maggiorenne");
+//         }
+//     }
+// }
